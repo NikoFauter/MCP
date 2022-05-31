38e62d72a603b3381d964cbce08965f9d866060b
nikofauter <nikofauter@uni-ulm.de>
2022-05-31 18:00:43 +0200

    first steps with the code

--- code/envsense/lib/bit.py
+++ code/envsense/lib/bit.py
@@ -1,29 +1,64 @@
 # Set the 'offset'-th bit in 'binary' to 1
+from cgi import test
+
+
 def set_bit(binary, offset):
+    binary = binary | 1 << offset
+    return binary
     pass
 
 
-# Set the 'offset'-th bit in 'binary' to 1
+# Set the 'offset'-th bit in 'binary' to 0
 def clear_bit(binary, offset):
+    test = binary & 1 << offset
+    if binary & 1 << offset != 0:
+        binary = binary ^ (1 << offset)
+    return binary
     pass
 
 
 # Toggle the 'offset'-th bit in 'binary' (if it was 0, set to 1 and vice-versa)
 def toggle_bit(binary, offset):
+    if (binary & 1 << offset == 0):
+        binary = set_bit(binary, offset)
+    else:
+        binary = clear_bit(binary, offset)
+    return binary
     pass
 
 
 # Return the 'offset'-th bit of 'binary'
 def get_bit(binary, offset):
+    if binary & 1 << offset == 0:
+        return 0
+    else:
+        return 1
     pass
 
 
 # Return all bits of 'binary' specified by 'mask' (marked with 1s)
 def get_mask(binary, mask):
+    return binary & mask
     pass
 
 
 # Set all bits of 'binary' specified by 'mask' to the corresponding
 # values of 'value'
 def set_mask(binary, mask, value):
+    i1 = 0
+    if mask < value:
+        raise ValueError
+    while 1 << i1 <= mask:
+        if(mask & 1 << i1 != 0):
+            if value & 1 << i1 == 0:
+                binary = clear_bit(binary, i1)
+            else:
+                binary = set_bit(binary, i1)
+
+        else:
+            if (mask & 1 << k1):
+                raise ValueError
+        k1 += 1
+        i1 += 1
+    return binary
     pass
--- code/envsense/lib/datatypes.py
+++ code/envsense/lib/datatypes.py
@@ -7,16 +7,44 @@ import ctypes
 
 
 def get_int_from_byte(binary):
+
+    if binary > 255:
+        raise ValueError
+
+    if binary & 1 << 7 != 0:
+        binary -= 1
+        inverted_binary = (~binary) & 0xFF
+
+        result = -int(inverted_binary)
+    else:
+        result = int(binary)
+    return result
     pass
 
 
 def get_int_from_ubyte(binary):
+    if binary > 256:
+        raise ValueError
+    return int(binary)
     pass
 
 
 def get_int_from_short(binary):
-    pass
+    if binary >= 1 << 16:
+        raise ValueError
+
+    if binary & 1 << 15 != 0:
+        binary -= 1
+        inverted_binary = (~binary) & 2**16-1
+
+        result = -int(inverted_binary)
+    else:
+        result = int(binary)
+    return result
 
 
 def get_int_from_ushort(binary):
+    if binary >= 1 << 16:
+        raise ValueError
+    return int(binary)
     pass
